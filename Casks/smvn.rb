961628c760b4ad1e673de6ae2dfa548d38e8f22d
Nguyen Van Manh <manhnv319@gmail.com>
2026-09-10 20:59:59 +0700

    refactor: use pure declarative cask DSL for Homebrew 6.0

--- Casks/smvn.rb
+++ Casks/smvn.rb
@@ -1,22 +1,13 @@
-require_relative "../lib/custom_download_strategy"
-
 cask "smvn" do
   version "0.1.2"
   sha256 "0f87f2bd11d84aee22ba833f93f1e8119c840c75c3657349a057ef36b33a2376"
 
-  url "https://github.com/manhnv319/smvn/releases/download/v#{version}/SMVN_#{version}_universal.dmg",
-      using: GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/manhnv319/smvn/releases/download/v#{version}/SMVN_#{version}_universal.dmg"
   name "SMVN"
   desc "Smart Model & Multi-Agent Orchestrator for macOS"
   homepage "https://github.com/manhnv319/smvn"
 
   app "SMVN.app"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/SMVN.app"],
-                   sudo: false
-  end
 
   zap trash: [
     "~/.omp/agent/config.yml",
