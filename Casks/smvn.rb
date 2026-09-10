8fb636403cb0381d78495f4ed21906a4917d8154
Nguyen Van Manh <manhnv319@gmail.com>
2026-09-11 00:07:42 +0700

    chore(cask): update smvn to 0.1.4

--- Casks/smvn.rb
+++ Casks/smvn.rb
@@ -1,8 +1,8 @@
 require_relative "../lib/custom_download_strategy"
 
 cask "smvn" do
-  version "0.1.3"
-  sha256 "18a9d457201f989cc24548f0a9efb8a11d9c21d8857505dbf6bb37a65fcb3643"
+  version "0.1.4"
+  sha256 "0de4ef94afdc8262844acbf6d63524e17a8b362b93262ecbe4f8f7bbbbf6bc6d"
 
   url "https://github.com/manhnv319/smvn/releases/download/v#{version}/SMVN_#{version}_universal.dmg",
       using: GitHubPrivateRepositoryReleaseDownloadStrategy
