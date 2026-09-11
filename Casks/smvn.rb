a927d961d07f43dff826e670a1fceb9e057fd7f4
Nguyen Van Manh <manhnv319@gmail.com>
2026-09-11 23:18:31 +0700

    chore(cask): update smvn to 0.2.2 (aarch64 local build)

--- Casks/smvn.rb
+++ Casks/smvn.rb
@@ -1,10 +1,10 @@
 require_relative "../lib/custom_download_strategy"
 
 cask "smvn" do
-  version "0.2.1"
-  sha256 "367d3c88b47f0a2bf16725fd476d0feb3c687696e53ad82f96ac6277f0000a2f"
+  version "0.2.2"
+  sha256 "8aba1b988629d7671f18f6d7528fca5491152014dc8b684cb446c37b7873d2a6"
 
-  url "https://github.com/manhnv319/smvn/releases/download/v#{version}/SMVN_#{version}_universal.dmg",
+  url "https://github.com/manhnv319/smvn/releases/download/v#{version}/SMVN_#{version}_aarch64.dmg",
       using: GitHubPrivateRepositoryReleaseDownloadStrategy
   name "SMVN"
   desc "Smart Model & Multi-Agent Orchestrator for macOS"
